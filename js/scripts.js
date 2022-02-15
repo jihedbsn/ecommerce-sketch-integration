489213d9c774e8bc43cdd15212e149ed5d1c440c
jihed <jihed.mohamed97@gmail.com>
2022-02-15 11:10:13 +0100

    slider completed

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).on("click", ".search", function () {
+  $(".search-bar").addClass("search-bar-active");
+});
+$(document).on("click", ".search-cancel", function () {
+  $(".search-bar").removeClass("search-bar-active");
+});
+
+
+/*login - sign -up      */
+$(document).on("click", ".account", function () {
+    $(".form").addClass("login-active");
+});
+
+$(document).on("click", ".sign-up-btn", function () {
+    $(".form").addClass("sign-up-active").removeClass("login-active");
+});
+
+$(document).on("click", ".form-cancel", function () {
+    $(".form").removeClass("sign-up-active").removeClass("login-active");
+});
+
+$(document).on("click", ".have-account", function () {
+    $(".form").addClass("login-active").removeClass("sign-up-active");
+});
+
+/* slider */
+$(document).ready(function() {
+  $('#adaptive').lightSlider({
+      adaptiveHeight:true,
+      item:1,
+      slideMargin:0,
+      loop:true,
+      auto:true
+  });
+});
